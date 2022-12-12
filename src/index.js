e3c3ff44b729c8f083a07ed8a919b566da90ca55
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2022-12-12 05:54:44 -0300

    Deconstructing package updates

--- src/index.js
+++ src/index.js
@@ -6,12 +6,10 @@ import * as h_inputClean from './helpers/input.js';
 import * as h_selectors from './helpers/selectors.js';
 import h_setTime from './helpers/set-time.js';
 import h_tokenGenerate from './helpers/token-generate.js';
-import * as h_updatePackages from './helpers/get-latest-version.js';
 
 export const { isEmpty, notEmpty } = h_empty;
 export const { s, sAll, sEl, sElAll } = h_selectors;
 export const { striptags, entities, xss } = h_inputClean;
-export const { getLatestVersion, updatePackages } = h_updatePackages;
 
 export const cx = h_cx;
 export const forceArray = h_forceArray;

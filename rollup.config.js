9970efb6c09fe0a77f662e855ea5208791b27a8a
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2023-01-05 05:37:33 -0300

    Adding typings to builder

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,32 @@
 // @ts-check
 
+import { defineConfig } from 'rollup';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 
-const setConfig = (/** @type {string} */ srcFile) => ({
-   input: `./src/${srcFile}.ts`,
-   external: ['html-entities'],
-   plugins: [
-      nodeResolve(),
-      commonjs(),
-      typescript({
-         tsconfig: './tsconfig.json',
-      }),
-   ],
-   output: [
-      {
-         file: `./${srcFile}.mjs`,
-         format: 'es',
-      },
-      {
-         file: `./${srcFile}.cjs`,
-         format: 'cjs',
-      },
-   ],
-});
+const setConfig = (/** @type {string} */ srcFile) =>
+   defineConfig({
+      input: `./src/${srcFile}.ts`,
+      external: ['html-entities'],
+      plugins: [
+         nodeResolve(),
+         commonjs(),
+         typescript({
+            tsconfig: './tsconfig.json',
+         }),
+      ],
+      output: [
+         {
+            file: `./${srcFile}.mjs`,
+            format: 'es',
+         },
+         {
+            file: `./${srcFile}.cjs`,
+            format: 'cjs',
+         },
+      ],
+   });
 
 export default [
    setConfig('index'),

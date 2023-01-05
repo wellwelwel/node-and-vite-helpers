17e77fba40484fa8236684efa660045dfbe9782d
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2023-01-05 00:59:21 -0300

    Fixing external dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,13 @@
+// @ts-check
+
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
    input: 'src/index.ts',
-   plugins: [typescript({ tsconfig: './tsconfig.json' }), nodeResolve({ jsnext: true }), commonjs()],
+   external: ['html-entities'],
+   plugins: [typescript({ tsconfig: './tsconfig.json' }), nodeResolve(), commonjs()],
    output: [
       {
          file: './index.cjs',
